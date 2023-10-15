74f5dc1378d3cb3ba31256493ececdae26821951
Dmitriy Datskevich <dimawarehouse@yahoo.com>
2023-10-15 13:25:37 -0400

    first

---
+++ features/steps/product_search.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+
+SEARCH_INPUT = (By.NAME, 'q')
+SEARCH_SUBMIT = (By.NAME, 'btnK')
+
+
+@given('Open Reely home page')
+def open_reely(context):
+    context.driver.get('https://soft.reelly.io/')
+
+@when('Log into the page')
+def click_login(context):
+    context.driver.find_element(*LOGIN_BUTTON).click()
+    sleep(1)
+
+@when('Input {search_word} into search field')
+def input_search(context, search_word):
+    search = context.driver.find_element(*SEARCH_INPUT)
+    search.clear()
+    search.send_keys(search_word)
+    sleep(4)
+
+
+@when('Click on search icon')
+def click_search_icon(context):
+    context.driver.find_element(*SEARCH_SUBMIT).click()
+    sleep(1)
+
+
+@then('Product results for {search_word} are shown')
+def verify_found_results_text(context, search_word):
+    assert search_word.lower() in context.driver.current_url.lower(), \
+        f'Expected query not in {context.driver.current_url.lower()}'
