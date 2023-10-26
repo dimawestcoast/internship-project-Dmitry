470ef01a5289428a710e6c07fff9c76be7ba9aec
Dmitriy Datskevich <dimawarehouse@yahoo.com>
2023-10-26 10:36:37 -0400

    Updated task 1

--- features/steps/product_search.py
+++ features/steps/product_search.py
@@ -1,36 +1,67 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 from behave import given, when, then
 from time import sleep
 
 
-SEARCH_INPUT = (By.NAME, 'q')
-SEARCH_SUBMIT = (By.NAME, 'btnK')
+LOGIN_EMAIL = (By.ID, 'email-2')
+LOGIN_PASSWORD = (By.ID, 'field')
 
 
 @given('Open Reely home page')
 def open_reely(context):
-    context.driver.get('https://soft.reelly.io/')
+    context.driver.get('https://soft.reelly.io/sign-in')
+    # context.driver.refresh()
 
 @when('Log into the page')
 def click_login(context):
-    context.driver.find_element(*LOGIN_BUTTON).click()
+    context.driver.find_element(*LOGIN_EMAIL).click()
+    sleep(2)
+    context.driver.find_element(*LOGIN_EMAIL).send_keys('dimawarehouse@yahoo.com')
+    sleep(2)
+    context.driver.find_element(*LOGIN_PASSWORD).send_keys('Mommalove@123')
+    element = WebDriverWait(context.driver,10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR,"a.login-button.w-button"))
+    )
+    element.click()
+    sleep(10)
+
+
+@when('Click on Connect Agency')
+def click_connect_agency(context):
+    element1 = context.driver.find_element(By.CSS_SELECTOR,
+    "div.get-free-period.menu")
+    element1.click()
+    sleep(2)
+
+
+@when('Switch the new tab')
+def switch_to_new_tab(context):
+    new_tab_handle = context.driver.window_handles[-1]
     sleep(1)
 
-@when('Input {search_word} into search field')
-def input_search(context, search_word):
-    search = context.driver.find_element(*SEARCH_INPUT)
-    search.clear()
-    search.send_keys(search_word)
-    sleep(4)
+
+@then('Verify there are 4 steps on the presentation page')
+def verify_4_steps(context):
+    steps = context.driver.find_element(By.CSS_SELECTOR, '[class="w-layout-grid steps-grid"]')
+    text = steps.text
+    print(text)
+    # visible_elements = 0
+    # for step in steps:
+    #     if step.is_displayed() and step.is_eneabled():
+    #         visible_elements += 1
+    # print(f'Number of visible steps found:{visible_elements}')
 
 
-@when('Click on search icon')
-def click_search_icon(context):
-    context.driver.find_element(*SEARCH_SUBMIT).click()
-    sleep(1)
 
 
-@then('Product results for {search_word} are shown')
-def verify_found_results_text(context, search_word):
-    assert search_word.lower() in context.driver.current_url.lower(), \
-        f'Expected query not in {context.driver.current_url.lower()}'
+@then('Verify Subscription Plans button is clickable')
+def verify_subscription_plan_clickable(context):
+    element2 = context.driver.find_element(By.XPATH, "//a[contains(@href, '/pro')]")
+
+    if element2.is_enabled():
+        print("Subscription plans button is clickable.")
+    else:
+        print("Subscription plans button is not clickable.")
